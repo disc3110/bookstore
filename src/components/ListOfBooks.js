d73127ae5257567d23f46e2e1e16fb9232466eae
Diego Solis <disc3110@gmail.com>
2021-08-24 19:20:09 -0500

    initialize project with components

---
+++ src/components/ListOfBooks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from './Book';
+
+const ListOfBooks = () => (
+  <table>
+    <tbody>
+      <Book />
+    </tbody>
+  </table>
+);
+
+export default ListOfBooks;
